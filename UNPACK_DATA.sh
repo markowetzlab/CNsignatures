6cec5a6153a82ff89295280c7549869fe4166537
Martingales <Ruben.Drews@cruk.cam.ac.uk>
2020-04-01 16:32:26 +0100

    added packed data files

---
+++ UNPACK_DATA.sh
@@ -0,0 +1,8 @@
+#!
+
+# To unpack data.tar:
+tar -xvf data.tar
+
+# To unpack manuscript_Rmarkdown.tar.part*:
+cat manuscript_Rmarkdown.tar.parta* > manuscript_Rmarkdown.tar
+tar -xvf manuscript_Rmarkdown.tar
